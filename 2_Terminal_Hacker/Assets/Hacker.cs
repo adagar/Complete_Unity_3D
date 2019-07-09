eda6d402c1f89a2fe4e27dd8255fe5560c691a45
Nick Garza <adagar121987@gmail.com>
2019-07-09 18:06:33 -0400

    Starting into section 2: Terminal Hacker

---
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello, console!");
+        Terminal.WriteLine("Welcome, Hacker...");
+        Terminal.WriteLine("How do you want to use your abilities?");
+        Terminal.WriteLine("\n");
+        Terminal.WriteLine("1 White - I hack to help");
+        Terminal.WriteLine("2 Gray - I hack to find");
+        Terminal.WriteLine("3 Black - I hack to hurt");
+        Terminal.WriteLine("\n");
+        Terminal.WriteLine("Choose Level to Begin: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
