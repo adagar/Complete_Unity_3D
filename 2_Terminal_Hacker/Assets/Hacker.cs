0f513d63d6ed04dd9dbf5fb4187066706640eaa6
Nick Garza <ngarza@cainc.com>
2019-07-21 19:15:27 -0400

    Basic password input and confirmation

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -8,6 +8,7 @@ public class Hacker : MonoBehaviour
     int level;
     enum Screen {MainMenu, Password, Win};
     Screen currentScreen = Screen.MainMenu;
+    string password;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +38,9 @@ public class Hacker : MonoBehaviour
         else if(currentScreen == Screen.MainMenu)
         {
             RunMainMenuInput(input);
+        } else if(currentScreen == Screen.Password)
+        {
+            RunCheckPassword(input);
         }
 
     }
@@ -54,11 +58,34 @@ public class Hacker : MonoBehaviour
         }
     }
 
+    void RunCheckPassword(string input)
+    {
+        switch (level)
+        {
+            case 1:
+                password = "white";                
+                break;
+            case 2:
+                password = "grey";
+                break;
+            default:
+                password = "black";
+                break;
+        }
+        if (input == password)
+        {
+            Terminal.WriteLine("Win!");
+        }
+        else
+        {
+            Terminal.WriteLine("Try again...");
+        }
+    }
+
     void StartGame()
     {
         Terminal.WriteLine("You have chosen level " + level);
         Terminal.WriteLine("Please enter your password...");
     }
-
 
 }
